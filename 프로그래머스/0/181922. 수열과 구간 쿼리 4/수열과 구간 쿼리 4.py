3af1f9fe10e18add494e12bf66a61d007dea6309
JeongJaeyeon <jaeyeonjeong62@gmail.com>
2026-09-01 17:04:28 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 5.43 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -0,0 +1,8 @@
+def solution(arr, queries):
+    answer = arr
+    for i in range(len(queries)):
+        for j in range(queries[i][0], queries[i][1]+1):
+            if j%queries[i][2]==0:
+                arr[j] += 1
+                
+    return answer
