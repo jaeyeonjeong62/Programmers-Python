bb7ef663480b8d80f0157038cdd7ee0cc12b9bc3
JeongJaeyeon <jaeyeonjeong62@gmail.com>
2026-09-01 17:56:32 +0900

    [level 0] Title: 배열 만들기 2, Time: 99.35 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,10 @@
+def solution(l, r):
+    answer = []
+    for i in range(l, r+1):
+        if "1" in str(i) or "2" in str(i) or "3" in str(i) or "4" in str(i) or "6" in str(i) or "7" in str(i) or "8" in str(i) or "9" in str(i):
+            continue
+        else:
+            answer.append(i)
+    if len(answer) == 0:
+        answer.append(-1)
+    return answer
