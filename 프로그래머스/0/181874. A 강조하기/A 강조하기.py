6294d8bab192dcf3f3e2c5b582af4817a080d586
JeongJaeyeon <jaeyeonjeong62@gmail.com>
2026-09-03 10:27:20 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -0,0 +1,10 @@
+def solution(myString):
+    answer = ''
+    for i in range(len(myString)):
+        if myString[i] == "a":
+            answer += "A"
+        elif myString[i] != "A":
+            answer += myString[i].lower()
+        else:
+            answer += myString[i]
+    return answer
